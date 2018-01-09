1c27d6cb64e1cd5683b536daf1415681c8405af8
Tony <790356596@qq.com>
2018-01-09 18:30:20 +0800

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+filename=`ls -l /root/study/log-backup/ | tail -n 1 | awk '{print $9}'`
+echo $filename
+
+cp /root/study/log-backup/$filename /root/study/logfactory/
+echo 'copy success'
+
+gunzip /root/study/logfactory/$filename
+echo 'unzip success' 
+
+file=${filename/\.gz/}
+echo $file
+
+mv /root/study/logfactory/$file /root/study/logfactory/out_log.log
+echo 'rename success'
+
+node /root/study/logfactory/main/start
+echo 'gen htmls success'
+
+rm -f /root/study/logfactory/out_log.log
+echo 'remove old log file success'
+
+echo 'all end'
