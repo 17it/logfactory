6751a5a1e8d2abed37827b41062d1c0cb10aeb1e
tangyongming <tangyongming@le.com>
2018-01-15 18:49:36 +0800

    添加汇总页面

--- main/sum.js
+++ main/sum.js
@@ -1,11 +1,64 @@
-var exec = require('child_process').exec;
-var cmdstr = "ls -l /root/study/hl | awk '{print$9}' > ./bb.log";
+var fs = require("fs");
+var path = require('path');
 
-exec(cmdstr, function(err,stdout,stderr){
-                                if(err) {
-                                    console.log('remove *.logs error: '+ stderr);
-                                    return;
-                                }
-                                console.log('remove *.logs success');
-                            });
+var transFile = {
+    init: function(){
+        this.head = '';
+        this.foot = '';
+        this.arr = '';
+        this.dstname = path.join(__dirname, '../../sum/sum.html');
+        this.initLogic();
+    },
+    initLogic: function(){
+        var self = this;
+        var _dir = path.join(__dirname, '../main/');
+        var exec = require('child_process').exec;
+        // "sort -k 2 -t '-' " + _dir + "_sum.log > " + _dir + "_year.log
+        var cmdstr = "ls -l " + path.join(__dirname, '../../hl') + " | awk '{print$9}' > _sum.log && sed -e 's/ *$/\",/' " + _dir + "_sum.log > " + _dir + "_sum-second.log && sed -e 's/^/\"/' " + _dir + "_sum-second.log > " + _dir + "_sum.log";
+        
+        console.log(cmdstr);
+        exec(cmdstr, function(err,stdout,stderr){
+            if(err) {
+                console.log('gen log error: '+ stderr);
+                return;
+            }
+            console.log(stdout);
+            self.genHtmls();
+        });
+    },
+    genHtmls: function(){
+        var _this = this;
+        
+        fs.readFile(path.join(__dirname,'../common/head-sum.html'), 'utf-8', function (err, data) {
+            if (err) throw err;
+            _this.head = data;
+            fs.readFile(path.join(__dirname,'../common/footer-sum.html'), 'utf-8', function (err, data) {
+                if (err) throw err;
+                _this.foot = data;
+                fs.readFile(path.join(__dirname, './_sum.log'), 'utf-8', function (err, data) {
+                    if (err) throw err;
+                    _this.arr = data;
+                    fs.writeFile(_this.dstname, _this.head + _this.arr + _this.foot, function (err) {
+                        if (err) throw err;
+                        console.log("create sum html success!");
+        
+                        var exec = require('child_process').exec;
+                        var cmdstr = 'rm -f ./main/_*.log';
+                        exec(cmdstr, function(err,stdout,stderr){
+                            if(err) {
+                                console.log('rm _*.log error: '+ stderr);
+                                return;
+                            }
+                            console.log(stdout);
+                        });
+                    });
+                });
+            });
+        });
+    }
+};
+
+transFile.init();
+
+
 
